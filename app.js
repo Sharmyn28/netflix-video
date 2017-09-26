2cadada7148c24cd85766897a1669dfa9c20984d
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-09-26 01:46:55 -0500

    classes and first design

--- app.js
+++ app.js
@@ -9,19 +9,54 @@ class App {
          selectedVideo: null,
          searchTerm: "iPhone X"
       }
-      this.youtubeSearch("iPhone X");
    }
+
+   init(){
+      this.youtubeSearch("iPhone X");
+      this.setup();
+   }
+
+   setup(){
+      $('#search').keyup(function(e) {
+            if(e.which == 13) {
+                  $("#root").empty();
+                  $('#mainVideo').empty();
+                  $('#mainVideo-details').empty();
+                  let search = $('#search').val();
+                  console.log(search);
+                  this.youtubeSearch(search);
+            }
+      });
+   }
+
+   getVideo(video){
+      const url = `http://www.youtube.com/embed/${video.id.videoId}`;
+      return `<iframe class="embed-responsive-item" src=${url}> </iframe>`;
+   }
+
+   getDetails(video){
+      const title = video.snippet.title;
+      const description = video.snippet.description;
+      return ` <p><b>${title}</b></p>
+                  <p>${description}</p>`;           
+    }
    //<iframe className="embed-responsive-item" src={url}> </iframe>
    getVideoList(videos) {
       return videos.map((video, index) => {
-         const imageUrl = video.snippet.thumbnails.default.url;
-         const url = `https://www.youtube.com/embed/${video.id.videoId}`;
-         return `<li> 
-                     <img class="media-object" src=${imageUrl} /> 
-                     <p> 
-                        <iframe class="embed-responsive-item" src=${url}> </iframe>
-                     </p>
-               </li>`;
+            const title = video.snippet.title;
+            const description = video.snippet.description;
+            const imageUrl = video.snippet.thumbnails.default.url;
+            const url = `https://www.youtube.com/embed/${video.id.videoId}`;
+            return `<li class="list-group-item">
+                        <div class='row'>
+                              <div class='col-md-6 col-ls-6 col-xs-6 col-sm-6'>
+                                    <img class="media-object" src=${imageUrl} /> 
+                              </div>
+                              <div class='col-md-6 col-ls-6 col-xs-6 col-sm-6'>
+                                    <p><b>${title}</b></p>
+                              </div>
+                        </div>
+                  </li>`;
       });
    }
 
@@ -34,9 +69,13 @@ class App {
             selectedVideo: data[0],
             searchTerm: searchTerm
          };
-         var list = this.getVideoList(this.result.videos);
+         let list = this.getVideoList(this.result.videos);
+         let mainVideo = this.getVideo(this.result.selectedVideo);
+         let mainVideoDetails = this.getDetails(this.result.selectedVideo);
          console.log("lis: ", list);
          $("#root").append(list);
+         $('#mainVideo').append(mainVideo)
+         $('#mainVideo-details').append(mainVideoDetails);
       });
    }
 
@@ -57,5 +96,6 @@ class App {
 
 $(document).ready(()=>{
       let app = new App();
+      app.init();
       }
 );
