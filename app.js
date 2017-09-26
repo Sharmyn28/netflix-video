8584e98e9adbef2ac835295bbd6c27d67ccb9226
Sharmyn28 <sharmynrodriguez@gmail.com>
2017-09-26 04:55:41 -0500

    slider

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ class App {
    setup(){
       $('#search').keyup(function(e) {
             if(e.which == 13) {
-                  $("#root").empty();
+                  $(".root").empty();
                   $('#mainVideo').empty();
                   $('#mainVideo-details').empty();
                   let search = $('#search').val();
@@ -37,23 +37,26 @@ class App {
    getDetails(video){
       const title = video.snippet.title;
       const description = video.snippet.description;
-      return ` <p><b>${title}</b></p>
+      return ` <h4 class='black-text'>${title}</h4>
                   <p>${description}</p>`;           
     }
    //<iframe className="embed-responsive-item" src={url}> </iframe>
    getVideoList(videos) {
       return videos.map((video, index) => {
             const title = video.snippet.title;
+            let titleFit = title;
+            titleFit = titleFit.split(' ');
+            let titleVideo = titleFit[0]+' '+titleFit[1];
             const description = video.snippet.description;
             const imageUrl = video.snippet.thumbnails.default.url;
             const url = `https://www.youtube.com/embed/${video.id.videoId}`;
-            return `<li class="list-group-item">
-                        <div class='row'>
-                              <div class='col-md-6 col-ls-6 col-xs-6 col-sm-6'>
-                                    <img class="media-object" src=${imageUrl} /> 
+            return `<li>
+                        <div class='panel panel-default'>
+                              <div class="panel-body">
+                                    <img class="media-object" src=${imageUrl} />                    
                               </div>
-                              <div class='col-md-6 col-ls-6 col-xs-6 col-sm-6'>
-                                    <p><b>${title}</b></p>
+                              <div class="panel-footer">
+                                    <p><b>${titleVideo}</b></p>
                               </div>
                         </div>
                   </li>`;
@@ -73,7 +76,7 @@ class App {
          let mainVideo = this.getVideo(this.result.selectedVideo);
          let mainVideoDetails = this.getDetails(this.result.selectedVideo);
          console.log("lis: ", list);
-         $("#root").append(list);
+         $(".root").append(list);
          $('#mainVideo').append(mainVideo)
          $('#mainVideo-details').append(mainVideoDetails);
       });
@@ -89,13 +92,15 @@ class App {
          };
          var list = this.getVideoList(this.result.videos);
          console.log("lis: ", list);
-         $("#root").append(list);
+         $(".root").append(list);
       });
    }
 };
 
 $(document).ready(()=>{
       let app = new App();
-      app.init();
-      }
+      app.init()},
+      $('#carousel').elastislide( {
+            minItems : 2
+      })
 );
